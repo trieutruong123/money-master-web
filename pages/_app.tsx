12249e2524b98512be865db9a6d1e7931c8c0367
truongtrieu123 <uerittruong@gmail.com>
2022-02-17 00:10:46 +0700

    update dashboard UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,46 @@
 import { NextPage } from 'next';
 import { AppProps } from 'next/app';
 import { AuthGuard } from 'components';
-
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import { createEmotionCache } from '../utils/create-emotion-cache';
+import { theme } from '../theme';
 import '../styles/globals.css';
-import { useEffect } from 'react';
-import { userService } from 'services';
-import { Router } from '@mui/icons-material';
 
-export type NextApplicationPage<P = any, IP = P> = NextPage<P, IP> & {
+type NextApplicationPage<P = any, IP = P> = NextPage<P, IP> & {
   requireAuth?: boolean;
+  getLayout?: any;
 };
+
+const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props: AppProps) {
   const {
     Component,
+    emotionCache = clientSideEmotionCache,
     pageProps,
-  }: { Component: NextApplicationPage; pageProps: any } = props;
+  }: {
+    Component: NextApplicationPage;
+    emotionCache?: EmotionCache;
+    pageProps: any;
+  } = props;
+  const getLayout = Component.getLayout ?? ((page: any) => page);
 
   return (
-    <>
-      {Component.requireAuth ? (
-        <AuthGuard>
-          <Component {...pageProps} />
-        </AuthGuard>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </>
+    <CacheProvider value={emotionCache}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {Component.requireAuth ? (
+            <AuthGuard>{getLayout(<Component {...pageProps} />)}</AuthGuard>
+          ) : (
+            getLayout(<Component {...pageProps} />)
+          )}{' '}
+        </ThemeProvider>
+      </LocalizationProvider>
+    </CacheProvider>
   );
 }
