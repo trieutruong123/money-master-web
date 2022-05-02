f4ed22945c095b8e0df1cae3448988c2e46d8dc4
Lâm Hoàng Đức <hoangducsmagic@gmail.com>
2022-05-03 00:29:07 +0700

    increase row gap

--- src/containers/currency-converter/index.tsx
+++ src/containers/currency-converter/index.tsx
@@ -177,14 +177,14 @@ const CurrencyConverter = observer(({ context }: IProps) => {
     >
       <Container
         maxWidth="lg"
-        sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+        sx={{ display: "flex", flexDirection: "row", alignItems: "center"}}
       >
         <Typography sx={{ mb: 3 }} align="center" variant="h4">
           CONVERTER
         </Typography>
         <Box sx={{ flexGrow: 1 }} />
       </Container>
-      <Container maxWidth={false}>
+      <Container maxWidth={false} sx={{display: "flex", flexDirection: "column", rowGap: "10px"}}>
         <AmountConvert
           sourceAmount={sourceAmount}
           targetAmount={targetAmount}
