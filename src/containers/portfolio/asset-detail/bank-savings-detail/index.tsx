fca00159a8ac7b616e2ac2f2f67bcde653797f16
Lâm Hoàng Đức <hoangducsmagic@gmail.com>
2022-06-02 03:00:23 +0700

    bank transaction: add transaction list

--- src/containers/portfolio/asset-detail/bank-savings-detail/index.tsx
+++ src/containers/portfolio/asset-detail/bank-savings-detail/index.tsx
@@ -17,23 +17,54 @@ import { UpdatedBankSavingItem } from "shared/types";
 import { AddNewTransactionModal } from "./add-new-transaction-modal";
 import { EditBankSavingsDetail } from "./edit-bank-savings-detail";
 
+import { TabPanel } from "shared/components";
+
+import { PABankBreadcrumbTabs } from "shared/constants/portfolio-asset";
+
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import React from "react";
+import TransactionHistory from './transaction-history'
+
 interface IProps {
   portfolioId: string;
   assetId: string;
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
 }
 
 export const BankSavingsDetail = observer(
   ({ portfolioId, assetId }: IProps) => {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+    useEffect(() => {
+      const fetchData = async () => {
+        Promise.all([
+          bankSavingsDetailStore.updateTransactionHistoryData(),
+        ]);
+      };
+      if (bankSavingsDetailStore.assetId && bankSavingsDetailStore.portfolioId) fetchData();
+    }, [bankSavingsDetailStore.assetId, bankSavingsDetailStore.portfolioId]);
 
-    const { assetDetail, isOpenAddNewTransactionModal } =
+    const { assetDetail, isOpenAddNewTransactionModal,transactionHistoryData } =
       bankSavingsDetailStore;
 
     const handleFormSubmit = async (data: UpdatedBankSavingItem) => {
       const res = await bankSavingsDetailStore.updateAssetDetail(data);
       return res;
     };
+
+    const [value, setValue] = React.useState(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+      setValue(newValue);
+    };
+
     return (
       <Box
         sx={{
@@ -54,14 +85,45 @@ export const BankSavingsDetail = observer(
           <Box sx={{ overflow: "hidden" }}>
             <Container sx={{ padding: isMobile ? "0px" : "initial" }}>
               <Grid container display="flex" justifyContent="center">
-                {typeof assetDetail !== "undefined" ? (
-                  <EditBankSavingsDetail
-                    assetDetail={assetDetail}
-                    handleFormSubmit={handleFormSubmit}
-                  />
-                ) : (
-                  <></>
-                )}
+              <Box sx={{ width: '100vw' }}>
+
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                  <Tabs
+                    value={value}
+                    onChange={handleTabChange}
+                    aria-label="basic tabs example"
+                  >
+                    <Tab
+                      label={PABankBreadcrumbTabs.overview}
+                      {...a11yProps(0)}
+                    />
+                    <Tab
+                      label={PABankBreadcrumbTabs.trnsactions}
+                      {...a11yProps(1)}
+                    />
+                    <Tab
+                      label={PABankBreadcrumbTabs.settings}
+                      {...a11yProps(2)}
+                    />
+                  </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                  {typeof assetDetail !== "undefined" ? (
+                    <EditBankSavingsDetail
+                      assetDetail={assetDetail}
+                      handleFormSubmit={handleFormSubmit}
+                    />
+                  ) : (
+                    <></>
+                  )}
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                  <TransactionHistory transactionHistoryData={transactionHistoryData}/>
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                  Settings
+                </TabPanel>
+                </Box>
               </Grid>
             </Container>
           </Box>
