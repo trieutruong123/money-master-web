56fc096ff1950016ecf4c80d2e77bf07aec5625b
Lâm Hoàng Đức <hoangducsmagic@gmail.com>
2022-06-02 01:57:44 +0700

    bankSaving transaction: make withdraw transaction

--- src/containers/portfolio/asset-detail/bank-savings-detail/index.tsx
+++ src/containers/portfolio/asset-detail/bank-savings-detail/index.tsx
@@ -1,10 +1,21 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Box, Container, Grid, useMediaQuery, useTheme } from '@mui/material';
-import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
-import { bankSavingsDetailStore } from 'shared/store';
-import { UpdatedBankSavingItem } from 'shared/types';
-import { EditBankSavingsDetail } from './edit-bank-savings-detail';
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Tooltip,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+
+import { bankSavingsDetailStore } from "shared/store";
+import { UpdatedBankSavingItem } from "shared/types";
+import { AddNewTransactionModal } from "./add-new-transaction-modal";
+import { EditBankSavingsDetail } from "./edit-bank-savings-detail";
 
 interface IProps {
   portfolioId: string;
@@ -14,9 +25,10 @@ interface IProps {
 export const BankSavingsDetail = observer(
   ({ portfolioId, assetId }: IProps) => {
     const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-    const { assetDetail } = bankSavingsDetailStore;
+    const { assetDetail, isOpenAddNewTransactionModal } =
+      bankSavingsDetailStore;
 
     const handleFormSubmit = async (data: UpdatedBankSavingItem) => {
       const res = await bankSavingsDetailStore.updateAssetDetail(data);
@@ -25,24 +37,24 @@ export const BankSavingsDetail = observer(
     return (
       <Box
         sx={{
-          display: 'flex',
-          height: '100%',
-          overflow: 'hidden',
-          width: '100%',
+          display: "flex",
+          height: "100%",
+          overflow: "hidden",
+          width: "100%",
         }}
       >
         <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flex: '1 1 auto',
-            overflow: 'hidden',
+            display: "flex",
+            justifyContent: "center",
+            flex: "1 1 auto",
+            overflow: "hidden",
           }}
         >
-          <Box sx={{ overflow: 'hidden' }}>
-            <Container sx={{ padding: isMobile ? '0px' : 'initial' }}>
+          <Box sx={{ overflow: "hidden" }}>
+            <Container sx={{ padding: isMobile ? "0px" : "initial" }}>
               <Grid container display="flex" justifyContent="center">
-                {typeof assetDetail !== 'undefined' ? (
+                {typeof assetDetail !== "undefined" ? (
                   <EditBankSavingsDetail
                     assetDetail={assetDetail}
                     handleFormSubmit={handleFormSubmit}
@@ -54,7 +66,23 @@ export const BankSavingsDetail = observer(
             </Container>
           </Box>
         </Box>
+        <Box>
+          <AddNewTransactionModal />
+        </Box>
+        <Tooltip title="Add new transaction">
+          <IconButton
+            onClick={() => {
+              bankSavingsDetailStore.setOpenAddNewTransactionModal(
+                !isOpenAddNewTransactionModal
+              );
+            }}
+            color="success"
+            sx={{ position: "absolute", right: "6vw", bottom: "6vh" }}
+          >
+            <AddCircleIcon sx={{ width: "4rem", height: "4rem" }} />
+          </IconButton>
+        </Tooltip>
       </Box>
     );
-  },
+  }
 );
